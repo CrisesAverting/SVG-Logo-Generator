6c3dcad11fd555d859efd36685c91a67fdea6f51
nicco <mannythemanmoth@gmail.com>
2023-06-12 15:19:42 -0400

    added test cases for generating SVG based on user specified shape

--- index.js
+++ index.js
@@ -1,6 +1,45 @@
-const inquirer = require("inquirer");
-const fs = require("fs");
+const inquirer = require('inquirer');
+const fs = require('fs');
+const jest = require('jest');
+const generateSVG = require("./lib/generateSVG.js")
 
-const shape = new Triangle();
-shape.setColor("blue");
-expect(shape.render()).toEqual('<polygon points="150, 18 244, 182 56, 182" fill="blue" />');
+const questions = [{
+    type: 'input',
+    name: 'logo_text',
+    message: 'Enter the text that will appear on the logo',
+},
+{
+    type: 'input',
+    name: 'logo_text_color',
+    message: 'Enter the color in hex or keyword format for the text ',
+},
+{
+    type: 'list',
+    name: 'logo_shape',
+    message: 'Select the shape in from the below list',
+    choices: ["Triangle", "Circle", "Square"],
+},
+{
+    type: 'input',
+    name: 'shapeColor',
+    message: 'Enter the color in hex or keyword format for the logo shape',
+
+},];
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) =>
+        err ? console.error(err) : console.log("Your SVG logo has been generated with your specifications")
+    );
+}
+
+// TODO: Create a function to initialize app
+function init() {
+    // request user specifications with inquirer
+    inquirer.prompt(questions).then((specs) => {
+        writeToFile("./Logo.svg", generateSVG(specs));
+        console.log(specs)
+    });
+}
+
+
+// Function call to initialize app
+init();
