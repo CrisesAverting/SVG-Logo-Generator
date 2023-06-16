d086a5ce2c0a2b44a0fb2a1ce68cd226e052e6a9
nicco <mannythemanmoth@gmail.com>
2023-06-15 20:24:17 -0400

    added length and basic none empty color check

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const jest = require('jest');
 const generateSVG = require("./lib/generateSVG.js")
-const check_length = (text) => {
+const checkLength = (text) => {
     if (text.length > 0 && text.length<4  ) {
         return true
     }
     return "Logo text cannot be 0 characters and cannot be more than 3 charaters"
 }
 
-const check_color = (color) => {
+const checkColor = (color) => {
     if (color!== ""){
         return true;
     }
@@ -20,13 +20,13 @@ const questions = [{
     type: 'input',
     name: 'logo_text',
     message: 'Enter the text that will appear on the logo',
-    validate: check_length,
+    validate: checkLength,
 },
 {
     type: 'input',
     name: 'logo_text_color',
     message: 'Enter the color in hex or keyword format for the text ',
-    validate: check_color,
+    validate: checkColor
 },
 {
     type: 'list',
@@ -38,7 +38,7 @@ const questions = [{
     type: 'input',
     name: 'shape_color',
     message: 'Enter the color in hex or keyword format for the logo shape',
-    validate: check_color,
+    validate: checkColor,
 
 },];
 function writeToFile(fileName, data) {
